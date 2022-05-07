87313cb5923870df13a67a2877e8cded779a176e
ambiti0n <fabulousfresh010@gmail.com>
2022-05-07 16:44:12 +0100

    UI setup complete

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import tw from "tailwind-styled-components";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <FooterContainer>
+        A limited NFT collection where the token itself doubles as your
+        membership to a swamp <br /> club for apes. The club is open! Ape in
+        with us.
+      </FooterContainer>
+    </Container>
+  );
 };
 
 export default Footer;
+const FooterContainer = tw.div`
+max-w-screen-lg
+w-full`;
+
+const Container = tw.div`
+flex
+justify-center
+my-5`;
--- components/Minting.js
+++ components/Minting.js
@@ -43,14 +43,22 @@ const Mint = tw.div`
  mt-[-200px]
 w-1/3
 z-50
+flex
+flex-col
+pb-4
+pr-4
 
 
 `;
 
 const ButtonContainer = tw.div`
+flex
+mt-2
+
 
 
 `;
 
 const FilledButton = tw.button`
+grow
 bg-[#bfc500] text-black hover:bg-white font-bold py-2 px-4 rounded uppercase`;
